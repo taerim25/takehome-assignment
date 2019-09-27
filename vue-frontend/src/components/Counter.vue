95cea7baac9b1f0acbbac683765794470217a92f
Geoffrey Taerim Kim <taerim25@gmail.com>
2019-09-27 15:00:34 -0400

    completed all parts except part4 and bonus for vue

--- vue-frontend/src/components/Counter.vue
+++ vue-frontend/src/components/Counter.vue
@@ -1,14 +1,29 @@
 <template>
-  <p>I'm a Counter!</p>
+  <div>
+    <p>I'm a Counter!</p>
+    <button v-on:click="initial_count += 1">Add 1</button>
+    <button v-on:click="initial_count -= 1">Subtract 1</button>
+    <p>Current count: {{initial_count}}</p>
+    <Instructions v-bind:complete="true"/>
+
+  </div>
 </template>
 
-<script>
+<script> 
+import Instructions from "./Instructions.vue";
+
 export default {
+  
+  props: {initial_count:{
+    default:0
+  } },
+  /*
   data() {
     return {
       count: 0
     };
-  }
+  }*/
+  
 };
 </script>
 
--- vue-frontend/src/components/Show.vue
+++ vue-frontend/src/components/Show.vue
@@ -1,11 +1,20 @@
 <template>
   <!-- PART 4: Add your code here -->
-  <div></div>
+  <!-- it doesn't seem to import Counter correctly, thus not showing the counter as intended-->
+  <div>
+    {{name}}
+    <Counter v-bind:initial_count = "episodes_seen"></Counter>
+  </div>
 </template>
 
 <script>
 // PART 4: Add your code here
-export default {};
+import Counter from "./Counter.vue";
+
+export default {
+  props:['id','name','episodes_seen']
+  
+};
 </script>
 
 <style>
